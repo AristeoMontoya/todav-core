4de3898229e1511903eff8c6f9edaa1a7c325e72
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-05-23 21:32:00 -0700

    Add watcher interface for configuration values

--- lib/src/main/java/todav/core/TodavCore.java
+++ lib/src/main/java/todav/core/TodavCore.java
@@ -1,7 +1,14 @@
 package todav.core;
 
+import todav.core.configuration.ConfigurationManager;
+
 public class TodavCore {
     public TodavClient initializeDefault() {
+        ConfigurationManager configManager = getConfigManager();
         return new TodavClient();
+    }
+
+    private ConfigurationManager getConfigManager() {
+        return null;
     }
 }
