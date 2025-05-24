00bbdd2bc4497bc7be85c65b0db9ed361b52de84
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-05-23 21:26:32 -0700

    Add initial configuration classes

--- lib/src/main/java/todav/core/TodavClient.java
+++ lib/src/main/java/todav/core/TodavClient.java
@@ -1,5 +1,26 @@
 package todav.core;
 
+import todav.core.configuration.ConfigurationManager;
+import todav.core.event.EventManager;
+
 public class TodavClient {
 
+    private ConfigurationManager configurationManager;
+    private EventManager eventManager;
+
+    public TodavClient() {
+
+    }
+
+    public String getConfigurationString() {
+        return null;
+    }
+
+    public int getConfigurationInt() {
+        return 0;
+    }
+
+    public boolean getConfigurationBoolean() {
+        return false;
+    }
 }
--- lib/src/main/java/todav/core/configuration/ConfigurationManager.java
+++ lib/src/main/java/todav/core/configuration/ConfigurationManager.java
@@ -1,4 +1,6 @@
 package todav.core.configuration;
 
 public class ConfigurationManager {
+    public ConfigurationManager(String some, String other, String thing, String idk) {
+    }
 }
